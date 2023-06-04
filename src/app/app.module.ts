683faed9434ac7caaeb068a42e237f4039c6932f
floatingnahid22 <nahid.uap.github@gmail.com>
2023-06-04 22:45:24 +0600

    did filter and pagination and also country card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CountryComponent } from './country/country.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CountryComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
+  declarations: [AppComponent, HomeComponent, CountryComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [HttpClientModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  countries: any = [];
+export class HomeComponent {
+  searchTerm: string = '';
 
-  constructor() {}
-
-  ngOnInit(): void {
+  searchCountries() {
+    if (this.searchTerm.trim() !== '') {
+      console.log('Search term:', this.searchTerm);
+    } else {
+      console.log('Empty search term');
+    }
   }
 }
